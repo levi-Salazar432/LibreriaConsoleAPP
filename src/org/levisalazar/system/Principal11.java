ee38d549b63b71f56726ff1a8c5bbd57cb248580
Levi Salazar <lsalazar/2026039@kinal.edu.gt>
2026-06-04 11:31:09 -0600

    000 - primer commit, configuración básica y .gitignore

---
+++ src/org/levisalazar/system/Principal11.java
@@ -0,0 +1,16 @@
+package org.levisalazar.system;
+
+/**
+ *
+ * @author Levi Salazar
+ */
+public class Principal11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
